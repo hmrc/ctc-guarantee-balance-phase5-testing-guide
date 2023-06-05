51d4c3e057521bddf460b3e75869272be0a40b09
fmcgrath <fergal.mcgrath@digital.hmrc.gov.uk>
2023-06-05 17:11:56 +0100

    Update bootstrap-play dependency

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,13 +2,17 @@ import play.sbt.PlayImport._
 import sbt._
 
 object AppDependencies {
-  val bootStrapPlayVersion = "7.12.0"
+  val bootStrapPlayVersion = "7.13.0"
 
   val compile: Seq[ModuleID] = Seq(
     ws,
-    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootStrapPlayVersion)
+    "uk.gov.hmrc" %% "bootstrap-frontend-play-28" % bootStrapPlayVersion
+  )
 
   val test: Seq[ModuleID] = Seq(
-    "org.pegdown" % "pegdown" % "1.6.0" % "test",
-    "org.scalatest" %% "scalatest" % "3.0.8" % "test")
+    "org.pegdown"          % "pegdown"                % "1.6.0"              % "test",
+    "com.vladsch.flexmark" % "flexmark-all"           % "0.36.8"             % "test",
+    "org.scalatest"       %% "scalatest"              % "3.2.9"              % "test",
+    "uk.gov.hmrc"         %% "bootstrap-test-play-28" % bootStrapPlayVersion % "test"
+  )
 }
